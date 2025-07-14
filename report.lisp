f79cda0bebc74436c35dd34182c6c593b4547840
Anthony Green <green@moxielogic.com>
2025-07-14 10:00:52 -0400

    Fix link

--- report.lisp
+++ report.lisp
@@ -726,7 +726,7 @@
                                            :if-does-not-exist :create)
         (markup:write-html-to-stream
          <page-template title="ubi-micro-dev" index="true">
-         ✨ <b>Start Here: <a href=\"https://github.com/ubi-micro-dev/ubi-micro-dev/blob/main/README.md\">Read About <code>ubi-micro-dev</code> Images</a></b> ✨
+         <a href=\"https://github.com/ubi-micro-dev/ubi-micro-dev/blob/main/README.md\">✨ <b>Start Here: Read About <code>ubi-micro-dev</code> Images</b> ✨</a>
          <br>
          <h2>Available Images</h2>
          <table class="table table-hover">
